897a1ea083421ff6c0a920f42a1ebc3567deeb4c
Ben Schultz <bens4lsu@gmail.com>
2023-05-10 14:15:57 -0500

    AppConfig() in config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -7,12 +7,12 @@ import LeafKit
 public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    let appConfig = DailyPhotoPublicFileManager.ac
+    let appConfig = AppConfig()
     app.http.server.configuration.port = appConfig.listenOnPort
     app.views.use(.leaf)
         
     
-    let x = try ImageGalleryPublicFileManager.getGalleries() //(atPath: "175 - Europe 2022 pt 2 - Coastal Croatia")
+    //let x = try ImageGalleryPublicFileManager.getGalleries() //(atPath: "175 - Europe 2022 pt 2 - Coastal Croatia")
     //print(x)
     // register routes
     try routes(app, appConfig)
