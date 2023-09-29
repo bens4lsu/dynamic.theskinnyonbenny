54a6e30c69e011d830d2a8a1a2aa95f70a7ee1ad
Ben Schultz <bens4lsu@gmail.com>
2023-09-29 13:44:43 -0500

    redirect /gal to static pages

--- Sources/App/Controllers/ImageGalleryRouteCollection.swift
+++ Sources/App/Controllers/ImageGalleryRouteCollection.swift
@@ -19,27 +19,18 @@ struct ImageGalleryRouteCollection: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let gal = routes.grouped("gal")
         
-        gal.get { req -> View in
-            let galleries = try ImageGalleryPublicFileManager.getGalleries()
-            return try await req.view.render("galleries", ["galleries" : galleries])
+        gal.get { req -> Response in
+            return req.redirect(to: "https://theskinnyonbenny.com/pgHome")
         }
         
-        gal.get(":id") { req -> View in
-            return try await req.view.render("gallery", loadGallery(req))
+        gal.get(":id") { req -> Response in
+            guard let parameter = req.parameters.get("id"),
+                  let id = Int(parameter)
+            else {
+                throw Abort (.badRequest, reason: "Gallery ID in request was not numeric.")
+            }
+            return req.redirect(to: "https://theskinnyonbenny.com/gal/\(id)")
         }
-        
-        gal.get("data", ":id") { req -> Response in
-            try await loadGallery(req).encodeResponse(for: req)
-        }
-    }
-    
-    private func loadGallery(_ req: Request) async throws -> Gallery {
-        guard let parameter = req.parameters.get("id"),
-              let id = Int(parameter)
-        else {
-            throw Abort (.badRequest, reason: "Gallery ID in request was not numeric.")
-        }
-        let gallery = try ImageGalleryPublicFileManager.getGallery(id)
-        return gallery
+
     }
 }
