80617b1a1a810b08702fbc4b6ae79b0f2fe804a4
Ben Schultz <bens4lsu@gmail.com>
2023-12-29 13:54:44 -0600

    removed files for img galleries

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,7 +6,6 @@ import Foundation
 
 func routes(_ app: Application, _ ac: AppConfig) throws {
     try? app.register(collection: DailyPhotoRouteCollection(ac))
-    try? app.register(collection: ImageGalleryRouteCollection(ac))
     
     app.get { req in
         return req.redirect(to: "https://theskinnyonbenny.com")
